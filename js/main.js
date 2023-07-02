694fdcd816fc90ce593af89933e23810bdd6b898
ElyPoli <itsos.poli@gmail.com>
2023-07-02 15:25:39 +0200

    fixed js code

--- js/main.js
+++ js/main.js
@@ -10,26 +10,39 @@ L’output del prezzo finale va messo fuori in forma umana (con massimo due deci
 */
 
 // Dichiaro variabili
-const kmToGo = prompt("Quanti chilometri vuoi percorrere?");
-const age = prompt("Inserisci la tua età");
+const kmToGo = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+const age = parseInt(prompt("Inserisci la tua età"));
+const ticketNumber = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+const date = new Date();
 const kmCost = 0.21;
-let ticketCost, sale;
+let ticketCost, sale, ticketTipe;
+
+// Controllo dati
+if (isNaN(kmToGo) || isNaN(age)) {
+    alert("Si è verificato un errore con i dati inseriti, ricarica la pagina.");
+}
 
 // Calcolo costo biglietto in base ai km
 ticketCost = parseInt(kmToGo) * kmCost;
 
-// Utente è minorenne o over 65? - applicazione sconto
-if (parseInt(age) < 18) {
+// Applicazione sconto
+if (age < 18) {
     sale = (ticketCost / 100) * 20;
     ticketCost = (ticketCost - sale).toFixed(2);
-} else if (parseInt(age) > 65) {
+    ticketTipe = "Bambino";
+} else if (age > 65) {
     sale = (ticketCost / 100) * 40;
     ticketCost = (ticketCost - sale).toFixed(2);
+    ticketTipe = "Over 65";
 } else {
     ticketCost = (ticketCost).toFixed(2);
+    ticketTipe = "Adulto";
 }
 
 // Stampo valori
 document.getElementById("km-to-go").innerHTML = kmToGo;
 document.getElementById("age").innerHTML = age;
 document.getElementById("ticket-cost").innerHTML = ticketCost;
+document.getElementById("ticket-tipe").innerHTML = ticketTipe;
+document.getElementById("ticket-number").innerHTML = ticketNumber;
+document.getElementById("date").innerHTML = (`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
